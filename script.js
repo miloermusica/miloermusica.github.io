a123accdb1ec5cc8fd7e0d179aa970a43872d996
miloermusica <miloermusica@gmail.com>
2024-11-29 23:48:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Función para sumar stock
 function sumarStock(idCelda) {
-  const celda = document.getElementById(idCelda);
-  let stockActual = parseInt(celda.innerText); // Obtiene el valor actual del stock
-  celda.innerText = stockActual + 1; // Suma 1 al valor actual
-}
+    const celda = document.getElementById(idCelda);
+    let stockActual = parseInt(celda.innerText); // Obtiene el valor actual del stock
+    celda.innerText = stockActual + 1; // Suma 1 al valor actual
+  }
+  
+  // Función para restar stock
+  function restarStock(idCelda) {
+    const celda = document.getElementById(idCelda);
+    let stockActual = parseInt(celda.innerText); // Obtiene el valor actual del stock
+    if (stockActual > 0) {
+      celda.innerText = stockActual - 1; // Resta 1 al valor actual
+    } else {
+      alert("El stock no puede ser menor a 0."); // Mensaje de error si ya está en 0
+    }
+  }
+  
